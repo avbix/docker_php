b0f44b5cce8751ba14a3ffa1e452afa7d50cd70f
anzhf <xandya@yandex.ru>
2023-10-24 22:38:32 +0300

    second

--- www/index.php
+++ www/index.php
@@ -1,20 +1,20 @@
 <?php
-// Установка имени пользователя и пароля для подключения к MySQL.
+// Setting the username and password for connecting to MySQL.
 $user = 'root';
 $pass = 'root';
 
-// Формирование строки DSN (Data Source Name), которая указывает на сервер и базу данных MySQL, а также указывает кодировку.
+// Creating a Data Source Name (DSN) that specifies the MySQL server, database, and character encoding.
 $dsn = 'mysql:host=mysql;dbname=information_schema;charset=utf8';
 
-// Создание экземпляра объекта PDO для соединения с базой данных.
+// Creating an instance of the PDO (PHP Data Objects) class to establish a database connection.
 $pdo = new PDO($dsn, $user, $pass);
 
-// Выполнение SQL-запроса для выборки данных из таблицы 'tables' в базе данных 'information_schema'.
+// Executing an SQL query to retrieve data from the 'tables' table in the 'information_schema' database.
 $stmt = $pdo->query('SELECT * FROM tables');
 
-// Извлечение первой строки результата запроса.
+// Fetching the first row of the query result.
 $row = $stmt->fetch();
 
-// Вывод результата запроса с помощью функции print_r(), чтобы просмотреть содержимое строки.
+// Printing the result of the query using the print_r() function to inspect the contents of the row.
 print_r($row);
 ?>
